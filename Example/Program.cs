ed5ff4043f1bfc9b4a95978fbde4d0985f79c7f0
Aibar Arpish <aibar201329@gmail.com>
2023-12-12 10:43:09 +0600

    Add new

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,5 +1,13 @@
 ﻿Console.WriteLine("2 Hello, World!");
-Console.WriteLine("2 Hello, World!");
-Console.WriteLine("2 Hello, World!");
-Console.WriteLine("2 Hello, World!");
-Console.WriteLine("2 Hello, World!");
+
+
+int firstNumber, secondNumber;
+firstNumber = 5;
+secondNumber = 6;
+
+if (firstNumber > secondNumber)
+{Console.WriteLine($"Первое число `{firstNumber}` больше чем второе число `{secondNumber}`");}
+if (firstNumber < secondNumber)
+{Console.WriteLine($"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`");}
+if (firstNumber == secondNumber)
+{Console.WriteLine($"Введенные числа равны `{firstNumber}`");}
