ca092df36033cb77f4bc62798b656d105c632b71
Aibar Arpish <aibar201329@gmail.com>
2023-12-15 17:38:30 +0600

    Add Seminar3 with Homework

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -85,47 +85,91 @@
 // }
 
 //#Dom1
+// Console.WriteLine("Dom1");
 // int[] arr = new int[10];
 
 // void FillArray(){
 //     for(int i=0; i<arr.Length; i++){
 //     arr[i] = new Random().Next(1,101);
-//     Console.Write($"\t'{arr[i]}'");
-//    }
+//     Console.Write($"\t'{arr[i]}'");}
 // }
 // FillArray();
 // Console.WriteLine();
-// FindArray(arr);
+// int answer = FindArray(arr);
+// Console.WriteLine($"Количество чисел в промежутке от 20 до 90: '{answer}'");
 
-// void FindArray (int[] array){
+
+// int FindArray (int[] array){
 //     int count =0;
 //     for(int i =0; i< array.Length; i++){
 //         if(array[i]>=20 && array[i]<=90){
 //         count++;}
 //     }
-//     Console.WriteLine($"Количество чисел в промежутке от 20 до 90: '{count}'");
+//     return count;
 // }
 
 //#Dom2
-// int[] arr = new int[10];
+// Console.WriteLine("\n Dom2");
+// int[] arr1 = new int[10];
 
-// void FillArray(){
-//     for(int i=0; i<arr.Length; i++){
-//     arr[i] = new Random().Next(1,50);
-//     Console.Write($"\t'{arr[i]}'");
+// void FillArray1(){
+//     for(int i=0; i<arr1.Length; i++){
+//     arr1[i] = new Random().Next(1,50);
+//     Console.Write($"\t'{arr1[i]}'");
 //    }
 // }
-// FillArray();
+// FillArray1();
 // Console.WriteLine();
-// FindArray(arr);
-// void FindArray (int[] array){
-//     int count =0;
+// FindArray1(arr1);
+// void FindArray1 (int[] array){
+//     int count1 =0;
 //     for(int i =0; i< array.Length; i++){
 //         if(array[i]%2==0){
-//         count++;}
+//         count1++;}
 //     }
-//     Console.WriteLine($"Количество четных чисел в массиве: '{count}'");
+//     Console.WriteLine($"Количество четных чисел в массиве: '{count1}'");
 // }
 
 //#Dom3
+// Console.WriteLine("\n Dom3");
+// double[] arr2 = new double[10];
 
+// void FillArray2(){
+//     for(int i=0; i<arr2.Length; i++){
+//     arr2[i] = new Random().NextDouble()*10;
+//     Console.Write($"\t'{Math.Round(arr2[i], 3)}'");
+//    }
+// }
+// FillArray2();
+// Console.WriteLine();
+// Console.WriteLine(FindDiference(arr2));
+
+// string FindDiference (double[] array){
+//     double max = array[0], min = array [0];
+//     for(int i =1; i< array.Length; i++){
+//         if(array[i]>max) max=array[i];
+//         if(array[i]<min) min=array[i];
+//     }
+//     double diff = max - min;
+//     return $"Максимальное значение {Math.Round(max, 3)} \n Минимальное значение {Math.Round(min, 3)} \n Разница между максимальным и минимальными элементами массива: {Math.Round(diff, 3)}";
+// }
+
+//#Dom4
+// Console.WriteLine("Dom4");
+// Console.Write("Введите число в диапазоне от 1 до 100 000: "); 
+// string num = Console.ReadLine();
+// int size = num.Length;
+// int number = Convert.ToInt32(num);
+
+// int[] array = new int[size];
+// MakeAndPrintArray(array);
+
+// void MakeAndPrintArray(int[] arr){
+// for(int i=arr.Length-1; i>=0; i--){
+//     arr[i] = number%10;
+//     number= number/10;
+// }
+// for(int i = 0; i<arr.Length; i++){
+//     Console.Write($"\t '{arr[i]}'");
+// }
+// }
